159d1ede3391bb652badfc4e60c201bcb890899b
imerelyjest <garymok@outlook.com>
2021-07-15 16:56:38 -0500

    Add files via upload

---
+++ character.py
@@ -0,0 +1,9 @@
+class MyPlayer:
+	def __init__(self, HP, STAM, 0, 0, 0, 0, 0, 0, VIT, END, TOUGH, STR, DEX, AGI, INT, WIL, FAITH, PER)
+	self.HP = HP
+	self.STAM = STAM
+	self.POSTURE = POSTURE
+	self.VIT = VIT
+	self.END = END
+	self.TOUGH = TOUGH
+	self.STR = STR
